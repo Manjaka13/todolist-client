c602c68b5902b928effca02ccb392fa4ad0898dc
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-01-29 19:36:14 +0100

    Client base project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import "./styles/App.scss";
+
+class App extends Component {
+	render() {
+		return (
+			<div className="td-app">
+			</div>
+		);
+	}
+}
+
+export default App;
