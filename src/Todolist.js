a9f23d8aa3e415c1f29b27eb16728b413a3a8b7e
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-01-29 20:45:32 +0100

    Todolist changed to class based component

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,36 +1,53 @@
-import React, {useState} from "react";
+import React, {Component} from "react";
 import "./styles/Todolist.scss";
 import {FontAwesomeIcon as Icon} from "@fortawesome/react-fontawesome";
 import {faPlusCircle} from "@fortawesome/free-solid-svg-icons";
 import Addform from "./Addform";
 
-function Todolist() {
-	const [addform_opened, set_addform_opened] = useState(false);
-	const date_change = (e) => {
+class Todolist extends Component {
+	constructor(props) {
+		super();
+		this.state = {
+			addform_opened: false
+		};
+		this.date_change = this.date_change.bind(this);
+		this.open_addform = this.open_addform.bind(this);
+		this.close_addform = this.close_addform.bind(this);
+	}
 
-	};
-	const open_addform = () => {
-		set_addform_opened(() => true);
-	};
-	const close_addform = () => {
-		set_addform_opened(() => false);
-	};
+	date_change(e) {
 
-	return (
-		<div className="td-list">
-			<div className="container">
-				<div className="head">
-					<p className="day">Day: </p>
-					<input className="date" type="date" value="2021-01-29" onChange={date_change} />
-					<p className="add" onClick={open_addform}>
-						<Icon className="icon" icon={faPlusCircle} /> Add new todo
-					</p>
+	}
+	
+	open_addform() {
+		this.setState({
+			addform_opened: true
+		});
+	}
+
+	close_addform() {
+		this.setState({
+			addform_opened: false
+		});
+	}
+
+	render() {
+		return (
+			<div className="td-list">
+				<div className="container">
+					<div className="head">
+						<p className="day">Day: </p>
+						<input className="date" type="date" value="2021-01-29" onChange={this.date_change} />
+						<p className="add" onClick={this.open_addform}>
+							<Icon className="icon" icon={faPlusCircle} /> Add new todo
+						</p>
+					</div>
+					<Addform opened={this.state.addform_opened} finished={this.close_addform} />
+					<p>Culpa exercitation ullamco anim irure enim ut ad dolore dolor duis dolor minim esse aliqua est voluptate aliqua aute.</p>
 				</div>
-				<Addform opened={addform_opened} finished={close_addform} />
-				<p>Culpa exercitation ullamco anim irure enim ut ad dolore dolor duis dolor minim esse aliqua est voluptate aliqua aute.</p>
 			</div>
-		</div>
-	)
+		);
+	}
 }
 
 export default Todolist;
